ef0eff15af943475b985819bfbcde2a4bcdbd996
George Raafat <114592858+5XGeorgeX5@users.noreply.github.com>
2024-01-02 17:23:51 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 /* eslint-disable no-unused-vars */
 
-export default function taskBlock (trueOrFalse) {
-  let task = false;
-  let task2 = true;
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
