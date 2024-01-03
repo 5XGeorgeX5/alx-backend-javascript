8288e12537c82e2c4f87ba8d821e9623123b5a75
George Raafat <114592858+5XGeorgeX5@users.noreply.github.com>
2024-01-03 18:14:22 +0200

    Create 1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  const isSuccess = {
+    status: 200,
+    body: 'Success',
+  };
+
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve(isSuccess);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
