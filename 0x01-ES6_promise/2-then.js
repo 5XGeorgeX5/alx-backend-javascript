3c8ab2bf936852b42df20f4a8b1f092c51874699
George Raafat <114592858+5XGeorgeX5@users.noreply.github.com>
2024-01-03 18:15:59 +0200

    Create 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  const isSuccess = {
+    status: 200,
+    body: 'success',
+  };
+
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return (isSuccess);
+    }).catch(() => (new Error()));
+}
