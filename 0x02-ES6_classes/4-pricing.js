b6b6f49254a8cb3043dd6cfedafbdba58c9ac2ac
George Raafat <114592858+5XGeorgeX5@users.noreply.github.com>
2024-01-04 22:51:47 +0200

    Create 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+// import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof (amount) !== 'number') {
+      throw TypeError('amount must be number');
+    }
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof (amount) !== 'number' && typeof (conversionRate) !== 'number') {
+      throw TypeError('amount & conversion rate must be number');
+    }
+
+    return amount * conversionRate;
+  }
+}
