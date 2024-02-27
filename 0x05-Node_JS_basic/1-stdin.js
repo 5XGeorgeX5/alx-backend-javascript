886537aa4ea6085356eb0a416558cd1cc9d19537
George Raafat <georgexraafat@gmail.com>
2024-02-27 14:19:07 +0200

    doing some task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on("data", (data) => {
+process.stdin.on('data', (data) => {
   const input = data.toString();
   process.stdout.write(`Your name is: ${input}`);
   process.exit();
 });
 
 if (!process.stdin.isTTY) {
-  process.on("exit", () => {
-    process.stdout.write("This important software is now closing\n");
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
   });
 }
