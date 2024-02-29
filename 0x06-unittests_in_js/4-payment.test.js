95e0aab07b5148a887c31677822c6f79a9fae2ed
George Raafat <georgexraafat@gmail.com>
2024-03-01 00:51:34 +0200

    doing some task

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,18 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber with type SUM and return 10', () => {
+    const calculateNumberSpy = sinon.spy(console, 'log');
+    const calculateNumberStub = sinon
+      .stub(Utils, 'calculateNumber')
+      .returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberSpy.calledWith('The total is: 10')).to.be.true;
+    calculateNumberStub.restore();
+    calculateNumberSpy.restore();
+  });
+});
